bc88ada06d826ea4ded6fae2031f62797c98471f
BugHunter-X <askzenai@gmail.com>
2024-12-30 14:10:06 +0100

    Add bugSolution.dart file

---
+++ bugSolution.dart
@@ -0,0 +1,20 @@
+```dart
+class MyClass {
+  int? _myVariable;
+
+  int get myVariable => _myVariable ?? 0; // Default value if null
+
+  set myVariable(int? value) {
+    _myVariable = value;
+  }
+}
+
+void main() {
+  MyClass obj = MyClass();
+  print(obj.myVariable); // Prints 0
+  obj.myVariable = 10;
+  print(obj.myVariable); // Prints 10
+  obj.myVariable = null; // Now this is allowed because of the updated setter
+  print(obj.myVariable); // Prints 0
+}
+```
